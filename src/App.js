30d0456cc9ac2961b3b5b803d4706bcf7c101853
Buzayehu Chiko <buzayehugirma12@gmail.com>
2025-06-22 10:55:25 -0400

    Added header and styling to EZTechMovie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
 import StreamList from './components/StreamList';
 import Movies from './components/Movies';
 import Cart from './components/Cart';
 import About from './components/About';
+import './App.css'; // ✅ Add this to use the styles below
 
 function App() {
   return (
     <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<StreamList />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="container">
+        <header className="header">
+          <h1>EZTechMovie</h1>
+          <p>
+            Welcome to EZTechMovie. This Movie store is operated by The Hollywood Theatre. It is
+            known for its vast collection of titles for rent, our knowledgeable staff ready to
+            recommend your new favorite film, and display cases full of authentic film props and
+            costumes from every era of movie making.
+          </p>
+        </header>
+
+        <Navigation />
+
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
+
+
+
